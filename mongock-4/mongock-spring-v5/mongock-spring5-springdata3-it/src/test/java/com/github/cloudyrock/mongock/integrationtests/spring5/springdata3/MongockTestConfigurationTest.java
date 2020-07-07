d02136cf3f1f1524aaa221e14a56a45e305c56c6
dieppa <aperezdieppa@gmail.com>
2020-07-07 09:44:19 +0100

    wip

--- mongock-4/mongock-spring-v5/mongock-spring5-springdata3-it/src/test/java/com/github/cloudyrock/mongock/integrationtests/spring5/springdata3/MongockTestConfigurationTest.java
+++ mongock-4/mongock-spring-v5/mongock-spring5-springdata3-it/src/test/java/com/github/cloudyrock/mongock/integrationtests/spring5/springdata3/MongockTestConfigurationTest.java
@@ -4,31 +4,32 @@ package com.github.cloudyrock.mongock.integrationtests.spring5.springdata3;
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongo3Driver;
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate;
 import com.github.cloudyrock.mongock.integrationtests.spring5.springdata3.changelogs.testConfiguration.TestConfigurationChangeLog;
+import com.github.cloudyrock.mongock.integrationtests.spring5.springdata3.client.ClientRepository;
 import com.github.cloudyrock.spring.v5.MongockTestConfiguration;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.bson.Document;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.GenericContainer;
 
-
-//TODO: Use testContainers here
-@EnableAutoConfiguration
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes = MongockTestConfigurationTest.ApplicationConfiguration.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(
+        classes = MongockTestConfigurationTest.ApplicationConfigurationInitializer.class,
+        initializers = MongockTestConfigurationTest.ApplicationConfigurationInitializer.class
+)
+@DataMongoTest
+@EnableMongoRepositories(basePackageClasses = ClientRepository.class)
 public class MongockTestConfigurationTest {
-
 
     @Autowired
     private SpringDataMongo3Driver mongockDriver;
@@ -37,13 +38,16 @@ public class MongockTestConfigurationTest {
 
     @Configuration
     @MongockTestConfiguration
-    public static class ApplicationConfiguration {
+    public static class ApplicationConfigurationInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-        @Bean
-        public MongoTemplate mongoTemplate() {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
             GenericContainer mongo = RuntimeTestUtil.startMongoDbContainer("mongo:4.2.0");
-            MongoClient mongoClient = MongoClients.create(String.format("mongodb://%s:%d", mongo.getContainerIpAddress(), mongo.getFirstMappedPort()));
-            return new MongoTemplate(mongoClient, mongoClient.getDatabase(RuntimeTestUtil.DEFAULT_DATABASE_NAME).getName());
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    applicationContext,
+                    String.format("spring.data.mongodb.uri=mongodb://%s:%d", mongo.getContainerIpAddress(), mongo.getFirstMappedPort()));
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    applicationContext, "spring.data.mongodb.database=" + RuntimeTestUtil.DEFAULT_DATABASE_NAME);
         }
     }
 
@@ -55,13 +59,9 @@ public class MongockTestConfigurationTest {
 
     @Test
     void shouldInjectMongockTemplateToContext_wheMongockTestConfigurationAnnotation() {
-
         new TestConfigurationChangeLog().testConfigurationWithMongockTemplate(mongockTemplate);
-
         Assertions.assertEquals(
                 1,
                 mongockTemplate.getCollection(TestConfigurationChangeLog.COLLECTION_NAME).countDocuments(new Document().append("field", "value")));
-
     }
-
 }
