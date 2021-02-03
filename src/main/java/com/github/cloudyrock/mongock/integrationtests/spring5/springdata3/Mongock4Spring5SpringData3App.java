9409a59049a4bbf310cfabcbb9b0fbf5e7cfdecb
dieppa <aperezdieppa@gmail.com>
2021-02-03 09:31:07 +0000

    changed structure

--- src/main/java/com/github/cloudyrock/mongock/integrationtests/spring5/springdata3/Mongock4Spring5SpringData3App.java
+++ src/main/java/com/github/cloudyrock/mongock/integrationtests/spring5/springdata3/Mongock4Spring5SpringData3App.java
@@ -1,42 +1,23 @@
 package com.github.cloudyrock.mongock.integrationtests.spring5.springdata3;
 
 
-import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongoV3Driver;
 import com.github.cloudyrock.mongock.integrationtests.spring5.springdata3.client.ClientRepository;
 import com.github.cloudyrock.mongock.integrationtests.spring5.springdata3.spring.DateToZonedDateTimeConverter;
 import com.github.cloudyrock.mongock.integrationtests.spring5.springdata3.spring.ZonedDateTimeToDateConverter;
 import com.github.cloudyrock.spring.v5.EnableMongock;
-import com.github.cloudyrock.spring.v5.MongockSpring5;
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.WriteConcern;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.WriteConcernResolver;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Using @EnableMongock with minimal configuration only requires changeLog package to scan
- * in property file
- *
- * RUN this application with dev profile
- */
 @EnableMongock
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = ClientRepository.class)
@@ -61,31 +42,6 @@ public class Mongock4Spring5SpringData3App {
         mongoTemplate.createCollection("clientCollection");
         return new MongoTransactionManager(mongoTemplate.getMongoDbFactory());
     }
-
-
-    /**
-     * This method has been modified in order to use Changock runner instead of Mongock(deprecated).
-     * This bean will be injected if SpringBoot application class(Mongock4Spring5SpringData3App) is not annotated with @EnableChangock
-     */
-    @Bean
-    @ConditionalOnMissingBean(MongockSpring5.MongockApplicationRunner.class)
-    @ConditionalOnExpression("${mongock.enabled:true}")
-    public MongockSpring5.MongockApplicationRunner mongockApplicationRunner(
-            ApplicationContext springContext,
-            MongoTemplate mongoTemplate,
-            ApplicationEventPublisher eventPublisher) {
-
-        return MongockSpring5.builder()
-                .setDriver(SpringDataMongoV3Driver.withDefaultLock(mongoTemplate))
-                .addChangeLogClass(com.github.cloudyrock.mongock.integrationtests.spring5.springdata3.changelogs.client.ClientUpdater2ChangeLog.class)
-                .addChangeLogsScanPackage("com.github.cloudyrock.mongock.integrationtests.spring5.springdata3.changelogs.client.initializer")
-                .setSpringContext(springContext)
-                .setEventPublisher(eventPublisher)
-                .buildApplicationRunner();
-    }
-
-
-
 
     @Bean
     public MongoCustomConversions customConversions() {
